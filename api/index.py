78ee119c5b3215a5248278c482499389a8f61e73
Andrew Wong <wongandrew97@gmail.com>
2022-12-14 02:49:31 -0500

    Validate sender doesn't work, but everything else works

--- api/index.py
+++ api/index.py
@@ -1,34 +1,15 @@
-from http.server import BaseHTTPRequestHandler
-import requests
-import json
 import datetime
+import json
+from http.server import BaseHTTPRequestHandler
+
+import requests
+
 
 class handler(BaseHTTPRequestHandler):
-
-    def validate(self):
-        # Validate uri path
-        if self.path != '/api':
-            self.send_response(404, f'{self.path} not found')
-            return
-        # Validate sender
-        src = self.headers.get('src')
-        print(f'src={src}')
-        if src != "Andrew's iPhone":
-            self.send_response(400, 'Bad request: improper issuer')
-            return
-
-    def get_available_times(self, times_dict):
-        print('Times dict: ' + str(times_dict))
-        return {t: v for t, v in times_dict.items() if v > 0}
-
-    def formatted_times(self, times):
-        formatted = []  # we use a list for performance benefit
-        for t, v in times.items():
-            formatted.append(f"{datetime.datetime.strptime(t, '%H%M').strftime('%I:%M %p')} has {v} slots open")
-        return '\n'.join(formatted)
-
     def do_GET(self):
         self.validate()
+
+        # We need an extra boolean because BaseHTTPRequestHandler does not support early exits
         start_date = self.headers.get('startDate')
         print(f'Received {start_date} header param')
         uri = f"https://xola.com/api/experiences/61536b244f19be5b3c6e4241/availability?" \
@@ -45,4 +26,27 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(formatted_times.encode('utf-8'))
-        return
+
+    def validate(self):
+        # Validate uri path
+        if self.path != '/api':
+            self.send_response(404, f'{self.path} not found')
+            self.end_headers()
+            return
+
+        # Validate sender
+        print(f"src={self.headers.get('src')}")
+        if self.headers.get('src') != 'a':
+            self.send_response(403, 'Forbidden: improper issuer')
+            self.end_headers()
+            return
+
+    def get_available_times(self, times_dict):
+        print('Times dict: ' + str(times_dict))
+        return {t: v for t, v in times_dict.items() if v > 0}
+
+    def formatted_times(self, times):
+        formatted = []  # we use a list for performance benefit
+        for t, v in times.items():
+            formatted.append(f"{datetime.datetime.strptime(t, '%H%M').strftime('%I:%M %p')} has {v} slots open")
+        return '\n'.join(formatted)
